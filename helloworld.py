7430a87aacf44a31f5067621b109d9fd3081af6d
bibekbasnet375 <bibekbasnet375@gmail.com>
2025-09-05 15:16:23 +0545

    first commit

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+import sys
+
+print(sys.version)
+print("hello it's me Bibek Basnet")
+print("&" * 5)
+price = 50
+print(price)
+print('hii')
+print(2)
+name = input("what is your name ? ")
+colour = input("which colour you like ? ")
+print(name + ' likes ' + colour)
+name = "Bibek basnet"
+age = 20
+is_newpatient = True
+print(name)
+print(age)
+print(is_newpatient)
+if 5 < 8:
+          print("five is less than eight")
+          if 2 < 6:
+           print("two is less than 6")
+           print(8)
---
+++ python variable.py
@@ -0,0 +1,31 @@
+#python variable
+#creating variables
+x= 5
+print(x)
+y= "bibek"
+print(y)
+x= "basnet"
+print(x)
+
+#casting
+x= str(5)
+y= int(5)
+z= float(5)
+print(x)
+print(y)
+print(z)
+
+#get the type
+x= 5
+y= "bibek"
+x= "ram"
+print(type(x))
+print(type(y))
+
+#case sensative
+a= 6
+A= "cat"
+print(a)
+print(A)
+
+
