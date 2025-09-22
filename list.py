7a67e969220dbbb112954ac2bc21b8dde5955a68
bibekbasnet375 <bibekbasnet375@gmail.com>
2025-09-22 15:49:41 +0545

    python-if..else

---
+++ list.py
@@ -0,0 +1,13 @@
+mylist = ["cow", "rat", "dog", "goat","dog"]
+print(mylist)
+print(len(mylist))
+list1= [1,2,3,4]
+print(list1)
+list2= ["ram", 34, True]
+print(list2)
+#access list item
+print(mylist[3])
+print(mylist[1:3])
+print(mylist[-3:-1])
+if "ram" in list2:
+    print("Yes,'ram' is in list2")
